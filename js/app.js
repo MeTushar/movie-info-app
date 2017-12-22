2eba339cc6e4131eb254a7dc81fbe538a431e22c
Tushar Mehrotra <tushar.mehrotra@unbxd.com>
2017-12-22 17:10:57 +0530

    pagination implementation

--- js/app.js
+++ js/app.js
@@ -8,26 +8,31 @@ function ready(handleSubmit) {
 
 ready(handleSubmit);
 
+const apiKey = "c447f21";
+
 function handleSubmit() {
   const form = document.querySelector("#handle-form");
   form.addEventListener("submit", function(e){
     e.preventDefault();
     let inputVal = form.querySelector("input").value;
-    const apiKey = "c447f21";
-    getMovies(inputVal, apiKey);
+    document.querySelector("#pagination-container").innerHTML = "";
+    getMovies(inputVal, apiKey, 1);
   })
 }
 
-function getMovies(inputVal, apiKey) {
-  let url = `https://www.omdbapi.com/?s=${inputVal}&apikey=${apiKey}`;
+function getMovies(inputVal, apiKey, pageNumber) {
+  let url = `https://www.omdbapi.com/?s=${inputVal}&apikey=${apiKey}&page=${pageNumber}`;
   axios.get(url)
     .then((response) => {
-      let data = response.data.Search;
-      let responseStatus = response.data.Response;
-      if(responseStatus === "True") {
-        createTemplate(data);
+      let totalResult = response.data.totalResults; //To calculate no. of pages in pagination
+      let data = response.data.Search; //To get the movie data to show
+      let responseStatus = response.data.Response; //To check if their is any response or not
+      if(responseStatus === "True") { //if their is response show the movies & pagination
+        createTemplate(data); // passing data to function creating template
+        createPagination(totalResult, inputVal); /* passing totalResult & search value to function creating pagination
+        */
       } else {
-        createTemplate(null);
+        createTemplate(null); //if their is no data show 404 no movie found
       }
     })
     .catch((err) => {
@@ -35,6 +40,7 @@ function getMovies(inputVal, apiKey) {
     });
 }
 
+//To render movies after getting data from api
 function createTemplate(movies) {
   let output = '';
   if(movies !== null) {
@@ -57,11 +63,37 @@ function createTemplate(movies) {
   document.querySelector("#movies").innerHTML = output;  
 }
 
+// To create pagination
+function createPagination(totalResults, inputVal) {
+  let numberOfPages = parseInt(totalResults/10) + 1 ; //Calculating number of pages to show
+  let paginationTemplate = "<ul class='pagination'>";
+  for(let i = 0; i < numberOfPages; i++) {
+    paginationTemplate += `
+      <li>
+        <a href="javascript:void(0);" onclick="handlePagination(this);" data-page="${i + 1}" data-search-val="${inputVal}">
+          ${i + 1}
+        </a>
+      </li>
+    `;
+  }
+  paginationTemplate += "</ul>";
+  document.querySelector("#pagination-container").innerHTML = paginationTemplate;
+}
+
+function handlePagination(item) {
+  let pageNumber = item.dataset.page; //storing current page number getting from data attribute
+  let searchValue = item.dataset.searchVal; //Search value getting from data attribute
+
+  getMovies(searchValue, apiKey, pageNumber); //Calling the function responsible to do api calls
+}
+
+//To change class of movie container when there is data or no data
 function changeClass(classAdd, classRemove) {
   document.querySelector("#movies").classList.add(classAdd);
   document.querySelector("#movies").classList.remove(classRemove);
 }
 
+//To remove hyphen from release year if present
 function removeHyphen(str) {
   if(str.charAt(str.length - 1) == "–") {
     str = str.slice(0, str.length - 1);
